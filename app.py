219059f75da77a9989528b702ae8c03e4d7b8d6b
Rax <damnloardbruh@gmail.com>
2019-09-08 11:28:57 +0700

    fffff

---
+++ app.py
@@ -0,0 +1,26 @@
+print('Hello World\n This is my very first python playground')
+
+print('Me is i like dog cuz it spell backwards as god , opp that was a joke\n')
+
+print('See, what i am tryin to do is filling my free time with printing those string\n')
+
+print('Get bored ?\n')
+
+print('OK let me show you something fun')
+
+print('''This is a dog 
+    0-----
+     ||||
+''')
+
+print('That enough About me , so what about you ? \n')
+country = raw_input('Where are you from ?\n')
+born = input('And what year you born ?\n')
+born = int(born)
+age = 2019 - born
+
+print("so you from " + country)
+
+print( "And let me guess your age ", age,"year old" )
+
+print("PUSHING GITHUB")
